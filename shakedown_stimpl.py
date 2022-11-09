160480623e238589508be356e5abac7169daf51c
jcain89 <cainj4@mail.uc.edu>
2022-11-09 16:19:42 -0500

    Changed to Multiply

--- shakedown_stimpl.py
+++ shakedown_stimpl.py
@@ -2,5 +2,5 @@ from stimpl.expression import *
 from stimpl.runtime import *
 
 if __name__=='__main__':
-  program = Print(Divide(IntLiteral(6),IntLiteral(4)))
+  program = Print(Multiply(IntLiteral(6),IntLiteral(4)))
   run_stimpl(program)
